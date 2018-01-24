fdcae587c70d9838ac7816f4a9542592649cc11a
duepunti <35503261+duepunti@users.noreply.github.com>
2018-01-24 19:42:00 +0100

    Add files via upload

---
+++ Tostapane/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Tostapane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toast = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // toast
+            // 
+            this.toast.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.toast.AutoSize = true;
+            this.toast.BackColor = System.Drawing.Color.Transparent;
+            this.toast.ForeColor = System.Drawing.Color.Aqua;
+            this.toast.Location = new System.Drawing.Point(91, 9);
+            this.toast.Name = "toast";
+            this.toast.Size = new System.Drawing.Size(51, 13);
+            this.toast.TabIndex = 0;
+            this.toast.Text = "DuePunti";
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Interval = 3000;
+            this.Timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(224, 33);
+            this.Controls.Add(this.toast);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DuePunti";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label toast;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ Tostapane/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tostapane
+{
+    public partial class Form1 : Form
+    {
+        String testo;
+        String sfondo;
+        String colore;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string[] args;
+            args = Environment.GetCommandLineArgs();
+
+            try
+            {
+                testo = args[1];
+                string finale = testo.Replace("*_*", " ");
+                toast.Text = finale;
+            }
+            catch
+            {
+                MessageBox.Show("Nessun Argomento Fornito", "DuePunti");
+            }
+
+            try
+            {
+                sfondo = args[2];
+                sfondo = sfondo.ToLower();
+
+                // ITALIANO
+                if(sfondo == "nero")
+                {
+                    this.BackColor = Color.Black;
+                }
+                else if(sfondo == "bianco")
+                {
+                    this.BackColor = Color.White;
+                }
+                else if(sfondo == "giallo")
+                {
+                    this.BackColor = Color.Yellow;
+                }
+                else if(sfondo == "arancione")
+                {
+                    this.BackColor = Color.Orange;
+                }
+                else if(sfondo == "verde")
+                {
+                    this.BackColor = Color.Green;
+                }
+                else if(sfondo == "rosso")
+                {
+                    this.BackColor = Color.Red;
+                }
+                else if(sfondo == "azzurro")
+                {
+                    this.BackColor = Color.LightBlue;
+                }
+                else if(sfondo == "fuchsia")
+                {
+                    this.BackColor = Color.Fuchsia;
+                }
+                else if(sfondo == "grigio")
+                {
+                    this.BackColor = Color.Gray;
+                }
+                else if(sfondo == "blu")
+                {
+                    this.BackColor = Color.Blue;
+                }
+
+                //  INGLESE
+                else if (sfondo == "white")
+                {
+                    this.BackColor = Color.White;
+                }
+                else if (sfondo == "yellow")
+                {
+                    this.BackColor = Color.Yellow;
+                }
+                else if (sfondo == "orange")
+                {
+                    this.BackColor = Color.Orange;
+                }
+                else if (sfondo == "green")
+                {
+                    this.BackColor = Color.Green;
+                }
+                else if (sfondo == "red")
+                {
+                    this.BackColor = Color.Red;
+                }
+                else if (sfondo == "lightblue")
+                {
+                    this.BackColor = Color.LightBlue;
+                }
+                else if (sfondo == "fuchsia")
+                {
+                    this.BackColor = Color.Fuchsia;
+                }
+                else if (sfondo == "gray")
+                {
+                    this.BackColor = Color.Gray;
+                }
+                else if(sfondo == "blue")
+                {
+                    this.BackColor = Color.Blue;
+                }
+
+
+            }
+            catch { }
+
+            try
+            {
+                colore = args[3];
+                colore = colore.ToLower();
+
+                //  ITALIANO
+                if(colore == "bianco")
+                {
+                    toast.ForeColor = Color.White;
+                }
+                else if (colore == "nero")
+                {
+                    toast.ForeColor = Color.Black;
+                }
+                else if (colore == "giallo")
+                {
+                    toast.ForeColor = Color.Yellow;
+                }
+                else if (colore == "arancione")
+                {
+                    toast.ForeColor = Color.Orange;
+                }
+                else if (colore == "verde")
+                {
+                    toast.ForeColor = Color.Green;
+                }
+                else if (colore == "rosso")
+                {
+                    toast.ForeColor = Color.Red;
+                }
+                else if (colore == "azzurro")
+                {
+                    toast.ForeColor = Color.LightBlue;
+                }
+                else if (colore == "fuchsia")
+                {
+                    toast.ForeColor = Color.Fuchsia;
+                }
+                else if (colore == "grigio")
+                {
+                    toast.ForeColor = Color.Gray;
+                }
+                else if (colore == "blu")
+                {
+                    toast.ForeColor = Color.Blue;
+                }
+
+                // INGLESE
+                else if (colore == "white")
+                {
+                    toast.ForeColor = Color.White;
+                }
+                else if (colore == "yellow")
+                {
+                    toast.ForeColor = Color.Yellow;
+                }
+                else if (colore == "orange")
+                {
+                    toast.ForeColor = Color.Orange;
+                }
+                else if (colore == "green")
+                {
+                    toast.ForeColor = Color.Green;
+                }
+                else if (colore == "red")
+                {
+                    toast.ForeColor = Color.Red;
+                }
+                else if (colore == "lightblue")
+                {
+                    toast.ForeColor = Color.LightBlue;
+                }
+                else if (colore == "fuchsia")
+                {
+                    toast.ForeColor = Color.Fuchsia;
+                }
+                else if (colore == "gray")
+                {
+                    toast.ForeColor = Color.Gray;
+                }
+                else if (colore == "blue")
+                {
+                    toast.ForeColor = Color.Blue;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int len = toast.Text.Length;
+            int piu = len + 100;
+            this.Size = new Size(200, 33);
+            //this.ClientSize.Width
+            toast.Left = (this.ClientSize.Width - toast.Size.Width) / 2;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Close();
+        }
+    }
+}
